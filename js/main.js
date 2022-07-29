7fa5d425406a876ffda77a71661ad0bd04fd575f
Francisco Zapata <drfcozapata@gmail.com>
2022-07-29 15:54:13 -0400

    Lesson 5 finished: v-if and conditional render of search added

--- js/main.js
+++ js/main.js
@@ -4,14 +4,24 @@ const app = Vue.createApp({
 	data() {
 		return {
 			search: null,
+			result: null,
+			error: null,
 		};
 	},
 	methods: {
 		async doSearch() {
-			const res = await fetch(API + this.search);
-			const data = await res.json();
-			console.log(data);
-			this.search = null;
+			this.result = this.error = null;
+			try {
+				const res = await fetch(API + this.search);
+				if (!res.ok) throw new Error('User not found');
+				const data = await res.json();
+				console.log(data);
+				this.result = true;
+			} catch (error) {
+				this.error = error;
+			} finally {
+				this.search = null;
+			}
 		},
 	},
 });
