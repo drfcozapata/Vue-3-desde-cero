d0e4422ce1010a738d202dd873a2668f3fbb87dd
Francisco Zapata <drfcozapata@gmail.com>
2022-07-29 15:02:18 -0400

    Lesson 3 finished: doSearch method added to Vue instance

--- js/main.js
+++ js/main.js
@@ -1,15 +1,16 @@
 const API = 'https://api.github.com/users/';
 
-async function doSearch() {
-	const res = await fetch(API + 'drfcozapata');
-	const data = await res.json();
-	console.log(data);
-}
-
 const app = Vue.createApp({
 	data() {
 		return {
 			message: 'Hello Vue 3!',
 		};
+	},
+	methods: {
+		async doSearch() {
+			const res = await fetch(API + 'drfcozapata');
+			const data = await res.json();
+			console.log(data);
+		},
 	},
 });
