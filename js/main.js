4d831e48ad68c682dff2f1b0fcc41d1dab8308d6
Francisco Zapata <drfcozapata@gmail.com>
2022-07-29 14:51:42 -0400

    Lesson 2 finished: async function added

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
 const API = 'https://api.github.com/users/';
 
+async function doSearch() {
+	const res = await fetch(API + 'drfcozapata');
+	const data = await res.json();
+	console.log(data);
+}
+
 const app = Vue.createApp({
 	data() {
 		return {
