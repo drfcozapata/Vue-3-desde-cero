702aa3e76943636c3980b3789fbd08e6b31f7fb8
Francisco Zapata <drfcozapata@gmail.com>
2022-07-30 19:55:42 -0400

    Lesson 14 finished: cached HTTP request added

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 const API = 'https://api.github.com/users/';
 
+const requestMaxTimeMs = 300000;
+
 const app = Vue.createApp({
 	data() {
 		return {
@@ -13,14 +15,14 @@ const app = Vue.createApp({
 		const savedFavorites = JSON.parse(window.localStorage.getItem('favorites'));
 		if (savedFavorites?.length) {
 			const favorites = new Map(
-				savedFavorites.map(favorite => [favorite.id, favorite])
+				savedFavorites.map(favorite => [favorite.login, favorite])
 			);
 			this.favorites = favorites;
 		}
 	},
 	computed: {
 		isFavorite() {
-			return this.favorites.has(this.result.id);
+			return this.favorites.has(this.result.login);
 		},
 		allFavorites() {
 			return Array.from(this.favorites.values());
@@ -29,24 +31,53 @@ const app = Vue.createApp({
 	methods: {
 		async doSearch() {
 			this.result = this.error = null;
+
+			/* Para evitar volver a hacer búsquedas de perfiles que ya están en favoritos */
+			const foundInFavorites = this.favorites.get(this.search);
+
+			/* Establece el tiempo mínimo para tener que volver a hacer
+			la búsqueda del favorito para actualizar su información */
+			const shouldRequestAgain = (() => {
+				if (!!foundInFavorites) {
+					const { lastRequestTime } = foundInFavorites;
+					const now = Date.now();
+					return now - lastRequestTime > requestMaxTimeMs;
+				}
+				return false;
+			})(); // IIFE
+
+			if (!!foundInFavorites && !shouldRequestAgain) {
+				console.log('Found and we use the cached version');
+				return (this.result = foundInFavorites);
+			}
+
+			/* Si no está en favoritos o tiene más tiempo allí del 
+			establecido, realiza nuevamente la búsqueda */
 			try {
+				console.log('Not found or cached version is too old');
 				const res = await fetch(API + this.search);
 				if (!res.ok) throw new Error('User not found');
 				const data = await res.json();
 				console.log(data);
 				this.result = data;
+				foundInFavorites.lastRequestTime = Date.now(); // Fija la nueva fecha de adición
 			} catch (error) {
 				this.error = error;
+				setTimeout(() => {
+					this.error = '';
+				}, 3000);
 			} finally {
 				this.search = null;
 			}
 		},
 		addFavorite() {
-			this.favorites.set(this.result.id, this.result);
+			this.result.lastRequestTime =
+				Date.now(); /* Agrega la fecha en que fue agregado el favorito */
+			this.favorites.set(this.result.login, this.result);
 			this.updateStorage();
 		},
 		removeFavorite() {
-			this.favorites.delete(this.result.id);
+			this.favorites.delete(this.result.login);
 			this.updateStorage();
 		},
 		showFavorite(favorite) {
